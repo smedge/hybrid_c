7a62fbd3abe10dddeb2a34d547bd5524f626d315
smedge <smedge@gmail.com>
2015-07-16 01:39:09 -0500

    menu refactoring

--- src/mode_mainmenu.c
+++ src/mode_mainmenu.c
@@ -1,22 +1,17 @@
 #include "mode_mainmenu.h"
 
-static bool hoverNew = false;
-static bool hoverLoad = false;
+//static bool hoverNew = false;
+//static bool hoverLoad = false;
 static bool hoverExit = false;
+
+static bool clickBeginExit = false;
+
+static void update_exit_button(const Input *input, bool *quit);
+static void render_menu_text();
 
 void mode_mainmenu_update(const Input *input, const unsigned int ticks, bool *quit)
 {
-	if (input->mouseX >= 100 && input->mouseX <= 155 &&
-		input->mouseY >= 230 && input->mouseY <= 240) {
-		hoverExit = true;
-
-		if (input->mouseLeft)
-			*quit = true;
-	}
-	else {
-		hoverExit = false;
-	}
-
+	update_exit_button(input, quit);
 	cursor_update(input);
 }
 
@@ -25,7 +20,34 @@ void mode_mainmenu_render()
 	graphics_clear();
 	graphics_set_ui_projection();
 
+	render_menu_text();
+	cursor_render();
 
+	graphics_flip();
+}
+
+static void update_exit_button(const Input *input, bool *quit) {
+	if (input->mouseX >= 98 && input->mouseX <= 157 &&
+		input->mouseY >= 228 && input->mouseY <= 242) {
+		hoverExit = true;
+
+		// begin quititng on mouse down
+		if (input->mouseLeft) {
+			clickBeginExit = true;
+		}
+
+		// quit on mouse up
+		if (clickBeginExit && !input->mouseLeft) {
+			*quit = true;
+		}
+	}
+	else {
+		hoverExit = false;
+		clickBeginExit = false;
+	}
+}
+
+static void render_menu_text() {
 	glPushMatrix();
 
 	FTGLfont *font = ftglCreatePixmapFont("./resources/fonts/square_sans_serif_7.ttf");
@@ -47,7 +69,9 @@ void mode_mainmenu_render()
 	glRasterPos2f(100.0f, 220.0f);
 	ftglRenderFont(font, "LOAD", FTGL_RENDER_ALL);
 
-	if (hoverExit)
+	if (clickBeginExit)
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	else if (hoverExit)
 		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 	else
 		glColor4f(1.0f, 1.0f, 1.0f, 0.50f);
@@ -58,9 +82,4 @@ void mode_mainmenu_render()
 	ftglDestroyFont(font);
 
 	glPopMatrix();
-
-
-	cursor_render();
-
-	graphics_flip();
 }
