5183fa9c7cc943f6c6666edd8027ae91610a6efc
Jason Zahn <smedge@gmail.com>
2018-10-06 23:11:23 -0500

    new tunes

--- src/mode_gameplay.h
+++ src/mode_gameplay.h
@@ -1,12 +1,12 @@
 #ifndef MODE_GAMEPLAY_H
 #define MODE_GAMEPLAY_H
 
-#define GAMEPLAY_MUSIC_01_PATH "./resources/music/deadmau5_Analogical.mp3"
+#define GAMEPLAY_MUSIC_01_PATH "./resources/music/deadmau5_October.mp3"
 #define GAMEPLAY_MUSIC_02_PATH "./resources/music/deadmau5_Snowcone.mp3"
 #define GAMEPLAY_MUSIC_03_PATH "./resources/music/deadmau5_ArcadiaRemasteredAgain.mp3"
 #define GAMEPLAY_MUSIC_04_PATH "./resources/music/deadmau5_Avarita.mp3"
 #define GAMEPLAY_MUSIC_05_PATH "./resources/music/deadmau5_CatThrusterOriginal.mp3"
-#define GAMEPLAY_MUSIC_06_PATH "./resources/music/deadmau5_PettingZoo.mp3"
+#define GAMEPLAY_MUSIC_06_PATH "./resources/music/deadmau5_Monophobia_Original.mp3"
 #define GAMEPLAY_MUSIC_07_PATH "./resources/music/deadmau5_HeavyPettingZoo.mp3"
 
 #include "cursor.h"
